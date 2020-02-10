baaeaf1674c1fba2f343a503db54cec8a3012ffb
jpgonzalezomc <j.gonzalez@equifax.com>
2020-02-10 18:41:28 -0300

    ADD: Configuracion con base de datos

---
+++ config.py
@@ -0,0 +1,8 @@
+import os	
+
+secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+PWD = os.path.abspath(os.curdir)	
+
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@127.0.0.1:5432/prueba_location'.format(PWD)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
