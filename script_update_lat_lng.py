0965c3bf945dc858b733f1e7378391fc60b33f2d
jpgonzalezomc <j.gonzalez@equifax.com>
2020-02-10 18:42:17 -0300

    FIX: metodo para acceder a la base de datos

--- script_update_lat_lng.py
+++ script_update_lat_lng.py
@@ -1,14 +1,29 @@
 from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+import config
+
+from sqlalchemy import Boolean, Column , ForeignKey
+from sqlalchemy import DateTime, Integer, String, Text, Float
+from sqlalchemy.orm import relationship
+
+
+
 
 app = Flask(__name__)
-# configure your app
-
+app.config.from_object(config)
+db = SQLAlchemy(app)
 manager = Manager(app)
+
+class Location(db.Model):
+    __tablename__ = 'location'
+    id = Column(Integer, primary_key=True)
+
 
 @manager.command
 def hello():
-    print("hello")
+    for location in Location.query.all():
+        print(location.id)
 
 if __name__ == "__main__":
     manager.run()
