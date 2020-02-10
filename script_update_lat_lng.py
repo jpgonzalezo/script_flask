d74e5ba878fdc0c6fb23fb7e9f974e4db563f0ba
jpgonzalezo <juan.gonzalez.o@usach.cl>
2020-02-10 17:07:31 -0300

    Add files via upload

---
+++ script_update_lat_lng.py
@@ -0,0 +1,14 @@
+from flask_script import Manager
+from flask import Flask
+
+app = Flask(__name__)
+# configure your app
+
+manager = Manager(app)
+
+@manager.command
+def hello():
+    print("hello")
+
+if __name__ == "__main__":
+    manager.run()
