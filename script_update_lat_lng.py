dddd2ff2b063b26763e18777cc6d04356bf5bd14
jpgonzalezo <juan.gonzalez.o@usach.cl>
2020-02-10 22:28:08 -0300

    fix: script base

--- script_update_lat_lng.py
+++ script_update_lat_lng.py
@@ -3,27 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 import config
 
-from sqlalchemy import Boolean, Column , ForeignKey
-from sqlalchemy import DateTime, Integer, String, Text, Float
-from sqlalchemy.orm import relationship
-
-
-
-
 app = Flask(__name__)
 app.config.from_object(config)
-db = SQLAlchemy(app)
 manager = Manager(app)
-
-class Location(db.Model):
-    __tablename__ = 'location'
-    id = Column(Integer, primary_key=True)
-
+db = SQLAlchemy(app)
 
 @manager.command
-def hello():
+def generateAddress():
+    from models.location import Location
+
     for location in Location.query.all():
-        print(location.id)
+        print(location.latitude)
+        print(location.longitude)
 
 if __name__ == "__main__":
     manager.run()
