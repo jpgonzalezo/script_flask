a9eacc42a55765469f8e4ea79630cfda7a4f0b65
jpgonzalezomc <j.gonzalez@equifax.com>
2020-02-11 16:27:43 -0300

    update: toma posiciones de google maps y realiza los calculos respectivos

--- models/location.py
+++ models/location.py
@@ -1,10 +1,29 @@
 from script_update_lat_lng import db
-from sqlalchemy import Boolean, Column , ForeignKey
+from sqlalchemy import Boolean, Column , ForeignKey, create_engine
 from sqlalchemy import DateTime, Integer, String, Text, Float
 from sqlalchemy.orm import relationship
 
 class Location(db.Model):
-    __tablename__ = 'location'
+    __tablename__ = 'test_cyber_two'
+    __table_args__ = {"schema":"files"}
     id = Column(Integer, primary_key=True)
+    longitude = Column(String)
+    latitude = Column(String)
+    def __repr__(self):
+        return '<Team model {}>'.format(self.id)
+
+class AddressLatLong(db.Model):
+    __tablename__ = 'address_lat_long'
+    __table_args__ = {"schema":"files"}
+    id = Column(Integer, primary_key=True)
+    street_number = Column(String)
+    street_name = Column(String)
+    colonia = Column(String)
+    ciudad = Column(String)
+    state = Column(String)
+    postal_code = Column(String)
+    formatted_address = Column(String)
     latitude = Column(String)
     longitude = Column(String)
+    location_type = Column(String)
+    location_id = Column(Integer)
--- script_update_lat_lng.py
+++ script_update_lat_lng.py
@@ -1,7 +1,10 @@
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+import googlemaps
 import config
+import json, urllib.request
+
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -10,11 +13,62 @@ db = SQLAlchemy(app)
 
 @manager.command
 def generateAddress():
-    from models.location import Location
+    from models.location import Location, AddressLatLong
 
+    
+    total = Location.query.count()
+    dsad = db.session.query(AddressLatLong).count()
+    print(dsad)
+    count = 1
     for location in Location.query.all():
-        print(location.latitude)
-        print(location.longitude)
+        if len(location.latitude)>0 and len(location.longitude)>0:
+            print("Procesando punto ("+location.latitude+","+location.longitude+") "+str(count)+"/"+str(total))
+            url = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAH4Nw9lua0IwGVRjaJGOCBc12UQPbFagM&latlng='+location.latitude+','+location.longitude
+            ur = urllib.request.urlopen(url)
+            url_response = json.load(ur)
+            result = url_response['results'][0]
+            address = AddressLatLong() 
+            address.street_number = ""
+            address.street_name = ""
+            address.colonia = ""
+            address.ciudad = ""
+            address.state = ""
+            address.postal_code = ""
+            address.formatted_address = ""
+            address.latitude = ""
+            address.longitude = ""
+            address.location_type = ""
+
+            address.formatted_address = result["formatted_address"]
+            address.latitude = result["geometry"]["location"]["lat"]
+            address.longitude = result["geometry"]["location"]["lng"]
+            address.location_type = result["geometry"]["location_type"]
+            address.location_id = location.id
+            for component in result["address_components"]:
+                if "street_number" in component["types"]:
+                    address.street_number = component["long_name"]
+
+                elif "route" in component["types"]:
+                    address.street_name = component["long_name"]
+
+                elif "sublocality_level_1" in component["types"]:
+                    address.colonia = component["long_name"]
+
+                elif "locality" in component["types"]:
+                    address.ciudad = component["long_name"]
+                
+                elif "administrative_area_level_1" in component["types"]:
+                    address.state = component["long_name"]
+                
+                elif "postal_code" in component["types"]:
+                    address.postal_code = component["long_name"]           
+            db.session.add(address)
+            db.session.commit()
+        else:
+            print("Punto con id:"+str(location.id)+" ignorado, formato latitud,longitud invalido "+str(count)+"/"+str(total))
+        count = count + 1
+    
+
 
 if __name__ == "__main__":
     manager.run()
