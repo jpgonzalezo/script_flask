ab9c3614559725af7d20851e0a5c91f1bf0c6cc5
jpgonzalezomc <j.gonzalez@equifax.com>
2020-02-12 09:32:01 -0300

    UPDATE: Configuracion DB

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 PWD = os.path.abspath(os.curdir)	
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@127.0.0.1:5432/prueba_location'.format(PWD)
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@127.0.0.1:5432/nyg-batch-qa'.format(PWD)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
